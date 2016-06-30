82607df7dc086c26c03f719d532c83bf9e377190
Etienne Galea <egalea.11@gmail.com>
2016-06-30 20:03:41 +0200

    init

---
+++ mensfashion/mensfashion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mensfashion.Startup))]
+namespace mensfashion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
